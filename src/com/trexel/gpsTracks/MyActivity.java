f2a63ec8d99a8c86a2ac555a4eb0931edc4dd1aa
Amanda Trexel <atrexel1@gmail.com>
2014-11-02 21:52:50 -0800

    Logging Latitude & Longitude Points

--- src/com/trexel/gpsTracks/MyActivity.java
+++ src/com/trexel/gpsTracks/MyActivity.java
@@ -2,14 +2,26 @@ package com.trexel.gpsTracks;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+        Log.v("MainApp", "Setting up GPSManager Object");
+        GPSManager gps = new GPSManager(this);
+        if(gps.canGetLocation()){
+            // gps enabled
+            Log.d("MainApp", "canGetLocation() is true");
+            Log.d("MainApp", "Latitude: "+gps.getLatitude());
+            Log.d("MainApp", "Longitude: "+gps.getLongitude());
+        } // return boolean true/false
+        Log.v("MainApp", "Finished canGetLocation if Statement");
+
+
     }
 }
