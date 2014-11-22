2e267f79d9c5e6079c7e683f8639f685d6d2fbb0
Amanda Trexel <atrexel1@gmail.com>
2014-11-22 15:27:01 -0800

    adding interaction with user to control location services

--- src/com/trexel/gpsTracks/MyActivity.java
+++ src/com/trexel/gpsTracks/MyActivity.java
@@ -1,15 +1,26 @@
 package com.trexel.gpsTracks;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.opencsv.CSVWriter;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -27,6 +38,8 @@ public class MyActivity extends Activity {
     boolean isCleared = true;
     Thread getCoordinates;
     double totalMiles = 0.0;
+    double totalNaticalMiles = 0.0;
+    int indexing = 0;
 
     GPSManager gps;
     Location location;
@@ -36,9 +49,12 @@ public class MyActivity extends Activity {
     Button startTracks;
     Button stopTracks;
     Button clearTracks;
+    Button printTracks;
     TextView gpsStatusTextView;
     TextView mileageTextView;
+    TextView indexTextView;
     EditText displayEditText;
+    AlertDialog.Builder settingsAlert;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,14 +67,16 @@ public class MyActivity extends Activity {
         startTracks = (Button) findViewById(R.id.startTracksButton);
         stopTracks = (Button) findViewById(R.id.stopTracksButton);
         clearTracks = (Button) findViewById(R.id.clearMilesButton);
+        printTracks = (Button) findViewById(R.id.printButton);
         gpsStatusTextView = (TextView) findViewById(R.id.gpsState);
         mileageTextView = (TextView) findViewById(R.id.mileageTextView);
+        indexTextView = (TextView) findViewById(R.id.indexTextView);
         displayEditText = (EditText) findViewById(R.id.displayEditText);
 
         Log.v("MainApp", "Setting up GPSManager Object");
-        gps = new GPSManager(this);
+        gps = new GPSManager(MyActivity.this);
 
-        //check if gps is enabled
+        //check if user has already enabled gps
         if(gps.isGPSEnabled && gps.isNetworkEnabled){
             //means gps is on and it found providers for GPS and Network
             gpsStatusTextView.setText("location is enabled");
@@ -71,6 +89,7 @@ public class MyActivity extends Activity {
             startTracks.setEnabled(true);
             stopTracks.setEnabled(true);
             clearTracks.setEnabled(true);
+            printTracks.setEnabled(true);
         }
         else{
             gpsStatusTextView.setText("location service is disabled");
@@ -79,21 +98,34 @@ public class MyActivity extends Activity {
             startTracks.setEnabled(false);
             stopTracks.setEnabled(false);
             clearTracks.setEnabled(false);
+            printTracks.setEnabled(false);
             Toast.makeText(getApplicationContext(), "GPS and Network is disabled", Toast.LENGTH_SHORT).show();
         }
 
         locationToggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(), "Clicked GPS on Button", Toast.LENGTH_SHORT).show();
-
+                gps = new GPSManager(getApplicationContext());
                 if (gps.isGPSEnabled && gps.isNetworkEnabled) {
                     Toast.makeText(getApplicationContext(), "Already Enabled", Toast.LENGTH_SHORT).show();
+                    getPosition.setEnabled(true);
+                    startTracks.setEnabled(true);
+                    stopTracks.setEnabled(true);
+                    clearTracks.setEnabled(true);
+                    printTracks.setEnabled(true);
+
+                    settingsAlert.show();
                 } else {
+                    //disable user from clicking location dependant buttons
+                    getPosition.setEnabled(false);
+                    startTracks.setEnabled(false);
+                    stopTracks.setEnabled(false);
+                    clearTracks.setEnabled(false);
+                    printTracks.setEnabled(false);
                     //need to turn on gps; must ask user to do this in settings
                     gpsStatusTextView.setText("location service is disabled");
                     Toast.makeText(getApplicationContext(), "Please enable location", Toast.LENGTH_SHORT).show();
-                    gps.showSettingsAlert();
+                    settingsAlert.show();
                 }
             }
         });
@@ -116,6 +148,7 @@ public class MyActivity extends Activity {
                     startTracks.setEnabled(false);
                     stopTracks.setEnabled(false);
                     clearTracks.setEnabled(false);
+                    printTracks.setEnabled(false);
                     Toast.makeText(getApplicationContext(), "Please enable location", Toast.LENGTH_SHORT).show();
                     gps.showSettingsAlert();
                 }
@@ -147,6 +180,7 @@ public class MyActivity extends Activity {
                     startTracks.setEnabled(false);
                     stopTracks.setEnabled(false);
                     clearTracks.setEnabled(false);
+                    printTracks.setEnabled(false);
                 }
             }
         });
@@ -173,6 +207,7 @@ public class MyActivity extends Activity {
                     startTracks.setEnabled(false);
                     stopTracks.setEnabled(false);
                     clearTracks.setEnabled(false);
+                    printTracks.setEnabled(false);
                 }
             }
         });
@@ -187,7 +222,10 @@ public class MyActivity extends Activity {
                     //reset the mileageTextView, displayEditText, and totalMiles
                     mileageTextView.setText("0.0 mi");
                     displayEditText.setText("");
+                    indexTextView.setText("0");
+                    indexing = 0;
                     totalMiles = 0.0;
+                    totalNaticalMiles = 0.0;
                 }else{
                     gpsStatusTextView.setText("location service is disabled");
                     Toast.makeText(getApplicationContext(), "Location must be enabled", Toast.LENGTH_SHORT).show();
@@ -195,17 +233,91 @@ public class MyActivity extends Activity {
                     startTracks.setEnabled(false);
                     stopTracks.setEnabled(false);
                     clearTracks.setEnabled(false);
+                    printTracks.setEnabled(false);
                 }
             }
         });
 
+        printTracks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(gps.isGPSEnabled && gps.isNetworkEnabled) {
+                    Toast.makeText(getApplicationContext(), "Output points to File", Toast.LENGTH_SHORT).show();
+
+                    Log.v("csvWriter", "attemptint to write to file");
+                    //create a csv file of the coordinate points
+                    CSVWriter writer = null;
+                    try
+                    {
+                        Log.v("csvWriter", "Output filepath: "+Environment.getExternalStorageDirectory().getPath());
+                        Log.v("csvWriter", "Output filepath: "+Environment.getRootDirectory().getPath());
+                        writer = new CSVWriter(new FileWriter(Environment.getExternalStorageDirectory().getPath()+
+                                "/trackPoints.csv"), ',');
+                        //writer = new CSVWriter(new FileWriter(Environment.getRootDirectory().getPath()+
+                        //        "/trackPoints.csv"), ',');
+
+                        for (Coordinate point : coordinateList){
+                            String myPoint = "";
+                            myPoint = point.latitude+",";
+                            myPoint += point.longitude+",";
+                            myPoint += point.timestamp+"";
+                            String[]entries = myPoint.split(",");
+                            writer.writeNext(entries);
+                        }
+                        writer.close();
+
+                        //String[] entries = "first#second#third".split("#"); // array of your values
+                        //Log.v("csvWriter", "entries: "+entries[0]+", "+entries[1]+", "+entries[2]);
+                        //writer.writeNext(entries);
+                        //Log.v("csvWriter", "writer: "+writer);
+                        writer.close();
+                    }
+                    catch (IOException e)
+                    {
+                        Log.v("csvWriter", "ERROR:: "+e);
+                        //error
+                    }
+                    Log.v("csvWriter", "finished attempting to write to file");
+
+                }else{
+                    gpsStatusTextView.setText("location service is disabled");
+                    Toast.makeText(getApplicationContext(), "Location must be enabled", Toast.LENGTH_SHORT).show();
+                    getPosition.setEnabled(false);
+                    startTracks.setEnabled(false);
+                    stopTracks.setEnabled(false);
+                    clearTracks.setEnabled(false);
+                    printTracks.setEnabled(false);
+                }
+            }
+        });
+
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        //Yes button clicked
+                        MyActivity.this.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                }
+            }
+        };
+
+        settingsAlert = new AlertDialog.Builder(this);
+        settingsAlert.setMessage("Would you like to change your current location Setting?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener);
 
     }//end onCreate()
 
     private class getGPSCoordinates extends Thread implements Runnable {
 
         TextView threadMileageTextView = (TextView) findViewById(R.id.mileageTextView);
-        double indexing = 0.0;
+        TextView threadIndexTextView = (TextView) findViewById(R.id.indexTextView);
+        EditText threadDisplayEditText = (EditText) findViewById(R.id.displayEditText);
 
         public void run() {
             //System.out.println("Hello from a thread!");
@@ -215,9 +327,14 @@ public class MyActivity extends Activity {
             while(!isCleared) {
                 //user may want to resume tracking
                 while(pollGPS){
+                    //add a new coordinate
+
+
+
+                    //update the mileage
                     double miles = updateMilage();
-                    totalMiles = totalMiles + miles;
-                    indexing = indexing + 0.5;
+                    //totalMiles = totalMiles + miles;
+                    indexing = indexing + 1;
 
                     Log.v("UpdateMileage", "current miles: "+miles+" mi");
 
@@ -225,7 +342,10 @@ public class MyActivity extends Activity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            threadMileageTextView.setText(indexing+" mi");
+                            threadMileageTextView.setText(totalMiles+" mi");
+                            threadIndexTextView.setText(indexing+"");
+                            threadDisplayEditText.append("  ["+coordinateList.get(indexing-1).latitude+", "+
+                                    coordinateList.get(indexing-1).longitude+"]\n");
                         }
                     });
 
@@ -246,6 +366,21 @@ public class MyActivity extends Activity {
     public double updateMilage(){
         //first create a new Coordinate
         Coordinate newCoord = new Coordinate();
+
+        //Log.v("getCoordinates","\ncurrentGPS: "+location.getLatitude());
+        //Log.v("getCoordinates","currentGPS: "+location.getLongitude()+"\n");
+        //Log.v("getCoordinates","currentLocation: "+location.getLatitude());
+        //Log.v("getCoordinates","currentLocation: "+location.getLongitude()+"\n");
+        location = gps.getLocation();
+        Log.v("getCoordinates",".\n******  UPDATE LOCATION  ******\n.");
+        Log.v("getCoordinates","TOTAL MILES: "+totalMiles);
+        Log.v("getCoordinates","TOTAL MILES: "+totalNaticalMiles+"\n.");
+
+        Log.v("getCoordinates","currentGPS: "+location.getLatitude());
+        Log.v("getCoordinates","currentGPS: "+location.getLongitude()+"\n");
+        Log.v("getCoordinates","currentLocation: "+location.getLatitude());
+        Log.v("getCoordinates","currentLocation: "+location.getLongitude()+"\n\n");
+
 
         // add the current latitude and longitude and timestamp
         if(location != null) {
@@ -276,13 +411,36 @@ public class MyActivity extends Activity {
             double lon2 = coordinateList.get(index).longitude;
 
             //calculate current miles
-            miles = distance(lat1, lon1, lat2, lon2, 'M');
+            miles = distFrom(lat1, lon1, lat2, lon2);
+            totalNaticalMiles = totalNaticalMiles +
+                    (distance(lat1, lon1, lat2, lon2, 'M')*0.00062137)*Math.pow(10,3);
         }
         Log.v("UpdateMileage", "distance: "+miles);
         return miles;
     }//end updateMileage
 
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        gps = new GPSManager(MyActivity.this);
+        if(gps.isGPSEnabled && gps.isNetworkEnabled){
+            gpsStatusTextView.setText("location service is enabled");
+            gps = new GPSManager(this);
+            getCoordinates = new getGPSCoordinates();
+            location = gps.getLocation();
+            getPosition.setEnabled(true);
+            startTracks.setEnabled(true);
+            stopTracks.setEnabled(true);
+            clearTracks.setEnabled(true);
+            printTracks.setEnabled(true);
+        }else{
+            gpsStatusTextView.setText("location service is disabled");
+            Toast.makeText(getApplicationContext(), "Need to enable location", Toast.LENGTH_SHORT).show();
+        }
+    }//end onResume()
+
+    /* handled in onResume() now
     @Override
     public void onRestart() {
         super.onRestart();
@@ -295,15 +453,29 @@ public class MyActivity extends Activity {
             startTracks.setEnabled(true);
             stopTracks.setEnabled(true);
             clearTracks.setEnabled(true);
+            printTracks.setEnabled(true);
         }else{
             gpsStatusTextView.setText("location service is disabled");
             Toast.makeText(getApplicationContext(), "Need to enable location", Toast.LENGTH_SHORT).show();
         }
 
     }//end onRestart()
+    */
 
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        gps.stopUsingGPS();
+        getPosition.setEnabled(false);
+        startTracks.setEnabled(false);
+        stopTracks.setEnabled(false);
+        clearTracks.setEnabled(false);
+        printTracks.setEnabled(false);
+    }//end onPause()
 
+
+    /* handled in onPause() now
     @Override
     public void onStop() {
         super.onStop();
@@ -312,9 +484,25 @@ public class MyActivity extends Activity {
         startTracks.setEnabled(false);
         stopTracks.setEnabled(false);
         clearTracks.setEnabled(false);
+        printTracks.setEnabled(false);
     }//end onStop()
+    */
 
 
+
+    public static double distFrom(double lat1, double lng1, double lat2, double lng2) {
+        double earthRadius = 3958.75;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double dist = earthRadius * c;
+
+        return dist;
+    }
 
     /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
     /*:  This function calculates distance from latitude and longitude  :*/
@@ -346,6 +534,39 @@ public class MyActivity extends Activity {
     /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
     private double rad2deg(double rad) {
         return (rad * 180 / Math.PI);
+    }
+
+
+
+    ////DEPERECIATED THIS EXPLOITED BUG CODE -- used to handle GPS programmatically
+    public void turnGPSOn()
+    {
+        Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
+        intent.putExtra("enabled", true);
+        MyActivity.this.sendBroadcast(intent);
+
+        String provider = Settings.Secure.getString(MyActivity.this.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+        if(!provider.contains("gps")){ //if gps is disabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            MyActivity.this.sendBroadcast(poke);
+
+
+        }
+    }
+    // automatic turn off the gps
+    public void turnGPSOff()
+    {
+        String provider = Settings.Secure.getString(MyActivity.this.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+        if(provider.contains("gps")){ //if gps is enabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            MyActivity.this.sendBroadcast(poke);
+        }
     }
 
 }
